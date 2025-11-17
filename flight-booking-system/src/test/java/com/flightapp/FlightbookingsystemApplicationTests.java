8b179807a94e2a2b1edb73d69de3642726071f30
yme754 <134798911+yme754@users.noreply.github.com>
2025-11-17 16:03:03 +0530

    changed name

--- flight-booking-system/src/test/java/com/flightapp/FlightbookingsystemApplicationTests.java
+++ flight-booking-system/src/test/java/com/flightapp/FlightbookingsystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Appdemo1ApplicationTests {
+class FlightbookingsystemApplicationTests {
 
 	@Test
 	void contextLoads() {
